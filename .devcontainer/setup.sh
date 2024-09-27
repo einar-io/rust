26741bcd2b270b478bad186ec3ebc1ed492e75a8
Einar Rasmussen <einar-io@users.noreply.github.com>
2024-09-27 09:27:13 +0000

    Clean-up setup.sh

--- .devcontainer/setup.sh
+++ .devcontainer/setup.sh
@@ -13,22 +13,13 @@ apt-get install -y \
   openssl \
   ripgrep \
   tig \
-
+  fzf \
+  git-delta \
+  liquidprompt \
+  mold
 
 ## Install rustup and common components
 curl https://sh.rustup.rs -sSf | sh -s -- -y 
-rustup install nightly
+rustup update stable
 rustup component add rustfmt
-rustup component add rustfmt --toolchain nightly
-rustup component add clippy 
-rustup component add clippy --toolchain nightly
-
-cargo install cargo-expand
-cargo install cargo-edit
-
-## setup and install oh-my-zsh
-sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
-cp -R /root/.oh-my-zsh /home/$USERNAME
-cp /root/.zshrc /home/$USERNAME
-sed -i -e "s/\/root\/.oh-my-zsh/\/home\/$USERNAME\/.oh-my-zsh/g" /home/$USERNAME/.zshrc
-chown -R $USER_UID:$USER_GID /home/$USERNAME/.oh-my-zsh /home/$USERNAME/.zshrc
+rustup component add clippy
