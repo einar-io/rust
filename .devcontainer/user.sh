3bb1f05587530b485ac2c0f5657a6b97f7f19b55
Einar Rasmussen <einar-io@users.noreply.github.com>
2024-09-27 11:09:30 +0000

    Simplify

--- .devcontainer/user.sh
+++
@@ -1,6 +0,0 @@
-## Install rustup and common components
-#curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh -s -- -y
-#source $HOME/.cargo/env
-#rustup update stable
-#rustup component add rustfmt
-#rustup component add clippy
