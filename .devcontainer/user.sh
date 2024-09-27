52d267f381b093b60d1a86570c4afd80697ff332
Einar Rasmussen <einar-io@users.noreply.github.com>
2024-09-27 10:19:00 +0000

    fix: non-interactive rustup

--- .devcontainer/user.sh
+++ .devcontainer/user.sh
@@ -1,5 +1,5 @@
 ## Install rustup and common components
-curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
+curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh -s -- -y
 rustup update stable
 rustup component add rustfmt
 rustup component add clippy
